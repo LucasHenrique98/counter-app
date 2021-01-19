9f57e6406b031046c56207cc3442ad37c4596780
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-01-19 10:15:21 -0300

    RefactotingToHooksStyle

--- src/components/CounterContainer.js
+++ src/components/CounterContainer.js
@@ -1,38 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import css from './counterContainer.module.css';
 import MenusButton from './MenusButton';
 import PlusButton from './PlusButton';
 
-export default class CounterContainer extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentStep: 0,
-    };
-  }
-
-  handleClick = (clickType) => {
-    const { currentStep } = this.state;
-
-    this.setState({
-      currentStep: clickType === '+' ? currentStep + 1 : currentStep - 1,
-    });
+export default function CounterContainer() {
+  const [currentStep, setCurrentStep] = useState(0);
+  const handleClick = (clickType) => {
+    setCurrentStep(clickType === '+' ? currentStep + 1 : currentStep - 1);
   };
 
-  render() {
-    return (
-      <div className={css.divContainer}>
-        <h1>Counter App</h1>
-        <div>
-          <MenusButton onClick={this.handleClick} className={css.space} />
-          <Counter className={css.space} step={this.state.currentStep} />
-          <PlusButton onClick={this.handleClick} className={css.space} />
-        </div>
-
-        <h5 className={css.footer}>By: Lucas Henrique</h5>
+  return (
+    <div className={css.divContainer}>
+      <h1>Counter App</h1>
+      <div>
+        <MenusButton onClick={handleClick} className={css.space} />
+        <Counter className={css.space} step={currentStep} />
+        <PlusButton onClick={handleClick} className={css.space} />
       </div>
-    );
-  }
+
+      <h5 className={css.footer}>By: Lucas Henrique</h5>
+    </div>
+  );
 }
